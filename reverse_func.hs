536187250bba5c7bf173f87481c96c23a2e5a83f
Maxim Larionov <maxim.larionov@flatstack.com>
2015-05-27 10:52:29 +0300

    try to use own init and last

--- reverse_func.hs
+++ reverse_func.hs
@@ -1,8 +1,20 @@
 reverse' :: [a] -> [a]
 reverse' [] = []
 reverse' [x] = [x]
-reverse' xs = last xs : reverse' (init xs)
+reverse' xs = last xs : reverse' (init' xs)
+
+init' :: [a] -> [a]
+init' []  = error("This is not right, empty list is not allowed here")
+init' (x:[]) = [x]
+init' (x:xs:[]) = [x]
+init' (x:xs) = x: init' xs
+
+last' :: [a] -> Maybe a
+last' [] = Nothing
+last' (x:[]) =  Just x
+last' (x:xs) = last' xs
 
 -- last : takes last item
 -- init : takes list without last item
 -- simply go through list
+--  list can be anything
